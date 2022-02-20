3423bba60cdb26ec2bd1fb159fea1542abe62035
Elbert Chan <elb.chan@gmail.com>
2022-02-20 14:42:45 -0800

    modify README so it shows

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Therapist Web
+
+This is intended to be a website/webapp for therapy services.
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const App = () => {
   return (
     <>
-      Hello World
+      Hello this is a Therapist's website.
+
     </>
   );
 }
