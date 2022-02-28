c3fd332a6efcbd7be10450770a05d69b115b4068
Elbert Chan <elb.chan@gmail.com>
2022-02-28 14:26:54 -0800

    add testimonials section and styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,9 +19,21 @@ const App = () => {
           <h2>Neurodivergent autistic female BIPOC Asian therapist <em>for you</em>.</h2>
           <p>Explore how meeting with a neurodivergent, as opposed to neurotypical, therapist can benefit you as a neurodivergent human.</p>
           <div className="round-btn">Get in touch</div>
-
         </div>
       </div>
+      <section className="narrative">
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto soluta quia saepe, cupiditate quam consequatur nostrum itaque explicabo corporis numquam harum dolores doloremque quis id eaque quidem commodi dicta ex?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto soluta quia saepe, cupiditate quam consequatur nostrum itaque explicabo corporis numquam harum dolores doloremque quis id eaque quidem commodi dicta ex?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto soluta quia saepe, cupiditate quam consequatur nostrum itaque explicabo corporis numquam harum dolores doloremque quis id eaque quidem commodi dicta ex?</p>
+      </section>
+      <section className="testimonials">
+        <h1>What People Are Saying</h1>
+        <section className="quotes-container">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime suscipit explicabo voluptatibus aliquam, similique deserunt numquam quos quis asperiores omnis minima, nihil earum, quibusdam perferendis. Perferendis repellat recusandae eum fugit!</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit nisi molestiae saepe praesentium deserunt iste! Molestias ex saepe veniam ullam repellendus a enim, tempora nobis amet nihil libero facere inventore?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus voluptatum quidem praesentium sunt a necessitatibus, natus nobis, corrupti in adipisci quis soluta, eaque earum ducimus? Quam dolorum quisquam quaerat a!</p>
+        </section>
+      </section>
     </>
   );
 }
